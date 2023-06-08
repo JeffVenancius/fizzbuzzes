ce040f1a067d788c7022579be5aed255ae4e1a84
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2023-06-08 17:39:18 -0300

    Add files via upload

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,6 @@
-def modulate_form():
+def divisable_form(): # for comparison
     print('fizzbuzz with division')
+    print(0)
     for i in range(1,101):
         output = ""
         if not i % 3: # since false is 0
@@ -14,6 +15,7 @@ def no_division_form():
     print('fizzbuzz "without" division')
     fizz_counter = 0
     buzz_counter = 0
+    print(0)
     for i in range(1,101):
         output = ""
         fizz_counter += 1
@@ -28,5 +30,22 @@ def no_division_form():
             output = i
         print(output)
 
-divisable_form()
-no_division_form()
+def lookup_form():
+    print('lookup fizzbuzz')
+    print(0)
+    for i in range(1,101):
+        output = ""
+        last_digit = str(i)[-1]
+        if not i % 3:
+            output += "fizz"
+        if last_digit == '5' or last_digit == '0':
+            output += "buzz"
+        if not output:
+            output = str(i)
+
+        print(output)
+
+
+# divisable_form()
+# no_division_form()
+lookup_form()
